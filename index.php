118795fcdf407c7860e515654853b1aeaf60b9cf
songwut <songwut@enoactic.com>
2019-06-23 20:53:32 +0700

    prepare to send to remote-repository

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel ="stylesheet" type="text/css" href="resetStyle.css">
+    <link rel ="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+    <header>
+        <nav>
+            <div class="main-wrapper">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                </ul>
+
+                <div class="nav-login">
+                    <form>
+                        <input type = "text" name="uid" placeholder="Username/e-mail">
+                        <input type = "password" name="pwd" placeholder="password">
+                        <button type="submit" name="submit">Login</button>
+                    </form>
+
+                    <a href="signup.php">Sign Up</a>
+                </div>
+
+            </div>
+        </nav>
+    </header>
+
+    <section class = "main-container">
+        <div class="main-wrapper">
+            <h2> Home </h2>
+        </div>
+    </section>
+
+</body>
+</html>
